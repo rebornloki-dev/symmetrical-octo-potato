4b80b36de21416ea431031b3f7f17085fc06c0bd
rebornloki-dev <lydieakokpe431@gmail.com>
2026-05-06 05:46:06 +0100

    added colours and finished the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::{cmp::Ordering, io};
 use rand::Rng;
+use::colored::*;
 fn main() {
     println!("Guess the number!");
 
@@ -15,13 +16,19 @@ println!("Please input your guess.");
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,           
+        };
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Less => println!("{}","Too small!".red()),
+        Ordering::Greater => println!("{}","Too big!".red()),
+        Ordering::Equal => {
+            println!("{}","You win!".green());
+            break;
+        }
     }
       }
     

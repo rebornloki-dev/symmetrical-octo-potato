b79f03cb96406db81ee2703edf3f8e56481cbc74
rebornloki-dev <lydieakokpe431@gmail.com>
2026-05-07 05:48:46 +0100

    learned integers and Variables

---
+++ variables/Integers/src/main.rs
@@ -0,0 +1,11 @@
+fn main (){
+    // Integers
+
+    let a = 98_222; // Decimal
+    let b = 0xff; // Hex
+    let c = 0o77; // Octal
+    let d = 0b1111_0000; // Binary
+    let e = b'A'; // Byte (u8 only) 
+
+    println!("Decimal: {}", a);
+}
---
+++ variables/Variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let  x = 5;
+    println!("The value of x is: {}", x);
+
+    let x= "six seven";
+    println!("The value of x is: {}", x);
+
+}
